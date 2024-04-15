2e88914ffb8d532b605a6d5a6350b65417aace98
Raph <raphael.hao.alive@gmail.com>
2024-04-15 16:35:45 +0800

    feat: 完成 commit 阶段执行的任务：找到 flags 并执行操作

--- packages/react-reconciler/src/fiberFlags.ts
+++ packages/react-reconciler/src/fiberFlags.ts
@@ -4,3 +4,5 @@ export const NoFlags = 0b0000001;
 export const Placement = 0b0000010;
 export const Update = 0b0000100; // 更新属性
 export const ChildDeletion = 0b0001000; // 删除子节点
+
+export const MutationMask = Placement | Update | ChildDeletion;
